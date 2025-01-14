c45548e0878fea840dc653794467675fbd604522
TechSupport84 <trainingtech84@gmail.com>
2025-01-14 19:11:10 +0300

    added

---
+++ Csharp/Module01/lesson01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Intrinsics.Arm;
+
+class Program
+{
+    //Write()
+    //WriteLine("")
+    //variable
+    //type:  int  double  string ,float
+    //Python  :  name = "" age  =30, money = 3.3
+    // ReadLine(); read from user 
+    static void  Main(string[] args)
+    {
+        Console.WriteLine("Welcome to CodeWithJeancy");
+        Console.WriteLine("What is your name ? ");
+        string name =  Console.ReadLine();
+
+        Console.WriteLine("What is your Age ? ");
+        string age  = Console.ReadLine();
+
+        int validAge = int.Parse(age);
+
+
+        Console.WriteLine($"Your username is  {name} and  your age is  {validAge} years old");
+    }
+}
