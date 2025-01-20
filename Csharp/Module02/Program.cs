3d9cbd185b400c6552c615d3dcd07f255dc03e6b
TechSupport84 <trainingtech84@gmail.com>
2025-01-20 21:30:27 +0300

    added

---
+++ Csharp/Module02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+//for()  ,  while()  foreach()
+//for (type variable; condition ,End)
+class Program
+{
+   static void  Main(string[]args)
+   {
+
+    Console.WriteLine("What is your Value ? (x): > ");
+     string input  = Console.ReadLine();
+  
+     int x  = int.Parse(input);
+     int  y = 20;
+     Console.WriteLine("Code Tech  ");
+     Console.WriteLine("For Loop");
+     for(int i = 0; i < 11 ; i++)
+         {
+         Console.WriteLine($"ID number: {i}");
+         }
+
+    Console.WriteLine("While  loop (Do)");
+     do{
+
+        Console.WriteLine($"{x} is less than ${y}" );
+        x++;
+
+     }while(x <  y);
+   }
+
+
+}
